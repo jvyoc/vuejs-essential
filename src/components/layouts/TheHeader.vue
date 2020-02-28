0803cd0787493df29e79c14fdbbce1e77bf6dc57
jvyoc <yongcheng.chen@hotmail.com>
2020-02-28 17:55:11 +0800

    update github pages

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+
+          <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <a href="/" class="navbar-brand">
+
+            <span class="title" >{{ logo.title }}
+            <img :src="logo.src" :alt="logo.title"></span>
+        </a>
+      </div>
+
+
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}csm_teahauslogo_535907d536.png`,
+        title: 'VuejsCaff'
+      },
+
+      navList: ['社区', '头条', '问答', '教程'],
+      activeNavIndex: 0,
+       showCollapsedNav: false
+    }
+  },
+  beforeCreate() {
+    this.uploadsUrl = 'http://projects-chen.de/fileadmin/_processed_/c/2/'
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index
+    },
+    toggleNav() {
+      this.showCollapsedNav = !this.showCollapsedNav
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title { display:yes; color: blue}
+.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+</style>
